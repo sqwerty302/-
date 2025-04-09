04eb6f06d0f312733e79e531e1ca000e911e7fb4
sqwerty302 <134461182+sqwerty302@users.noreply.github.com>
2025-04-09 19:56:03 +1000

    task4.py

--- task4.py
+++ task4.py
@@ -1,4 +1,23 @@
-def count_unique_chars(s):
-    unique_chars = set(s)
-    return len(unique_chars)
-print(count_unique_chars("KrotenokEvgeny"))
+def find_min_max(arr):
+    if not arr:
+        raise ValueError("Массив пуст.")
+
+    min_val = max_val = arr[0]
+
+    for num in arr[1:]:
+        if num < min_val:
+            min_val = num
+        elif num > max_val:
+            max_val = num
+
+    return min_val, max_val
+
+
+try:
+    user_input = input("Введите элементы массива через пробел: ")
+    arr = list(map(int, user_input.strip().split()))
+    min_val, max_val = find_min_max(arr)
+    print(f"Минимальное значение: {min_val}")
+    print(f"Максимальное значение: {max_val}")
+except ValueError as e:
+    print("Ошибка:", e)
